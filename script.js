9eb1fa2b69ecb20e0bb192c911ba9b42025271a6
Mixer-sir <rmT13089@auca.kg>
2025-06-16 12:33:03 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.product-card');
+    const modal = document.getElementById('modal');
+    const modalTitle = document.getElementById('modal-title');
+    const modalDesc = document.getElementById('modal-desc');
+    const modalPrice = document.getElementById('modal-price');
+    const modalClose = document.getElementById('modal-close');
+  
+    cards.forEach(card => {
+      card.addEventListener('click', () => {
+        const name = card.dataset.name;
+        const desc = card.dataset.desc;
+        const price = card.dataset.price;
+  
+        modalTitle.textContent = name;
+        modalDesc.textContent = desc;
+        modalPrice.textContent = price;
+        
+        modal.style.display = 'flex';
+        const content = modal.querySelector('.modal__content');
+        content.style.animation = 'none';
+        void content.offsetWidth; // хак чтобы сбросить анимацию
+        content.style.animation = 'modalShow 0.4s ease-out forwards';
+
+
+        modal.style.display = 'flex';
+      });
+    });
+  
+    modalClose.addEventListener('click', () => {
+      modal.style.display = 'none';
+    });
+  
+    window.addEventListener('click', e => {
+      if (e.target === modal) modal.style.display = 'none';
+    });
+  });
+  
